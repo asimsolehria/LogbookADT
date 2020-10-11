7d382e20ab8af1b4a16fd271502691d6ccd7f956
Asim Habib <de.asimhabib@gmail.com>
2020-10-11 18:34:34 +0500

    Completed definition of methods

--- src/logbook/Coffee.java
+++ src/logbook/Coffee.java
@@ -7,5 +7,99 @@ package logbook;
  */
 public class Coffee {
     
+    int day;
+    public static void main(String[] args) 
+    {
+        
+        int day;
+        
+          Logbook coffee= new Logbook(12, 2000);
+          
+          System.out.println(coffee.day(2000, 12, 8));
+          
+//-------------------------Putting and getting Entry Test-----------------------
+          
+//        coffee.putEntry(2, 100);
+//        coffee.putEntry(15, 200);
+//        coffee.putEntry(15, 300);
+//        coffee.putEntry(1, 98);
+//        coffee.putEntry(coffee.daysInMonth(), 32);
+//        System.out.println(coffee.getEntry(2));
+//        System.out.println(coffee.getEntry(15));
+//        System.out.println(coffee.getEntry(1));
+//        System.out.println(coffee.getEntry(coffee.daysInMonth()));
+
+
+//-----------------------------Logbook entries print Test-----------------------
+
+//        System.out.println("Month/Year : "+coffee.getLogMonth()+"/"+coffee.getLogYear());
+//        for (day = 1; day <= coffee.daysInMonth(); day++) 
+//        {
+//            System.out.println(day + " : "+coffee.getEntry(day));   
+//        }
+
+
+//-----------------------------Day of Week print Test---------------------------
+
+//        System.out.println(coffee.daysInMonth());
+//        System.out.println(coffee.dayOfWeek(1));
+
+
+//------------------------------Default constructor Test------------------------
+
+//        Logbook softDrink=new Logbook();
+//
+//        System.out.println(softDrink.daysInMonth());
+//        softDrink.putEntry(36);
+//        System.out.println(softDrink.getEntry());
+
+
+//----------------Entries summation Test for a particular month-----------------
+
+//        Logbook softDrinks=new Logbook(12, 2000);
+//
+//        softDrinks.putEntry(2, 256);
+//        softDrinks.putEntry(15, 600);
+//
+//        softDrinks.plus(coffee);
+//
+//        System.out.println(softDrinks.getEntry(2));
+//        System.out.println(softDrinks.getEntry(15));
+
+
+
+//----------------Summation of logDay100 and logDay200 Entires------------------
+
+
+//        Logbook logDay100=new Logbook(12, 2000);
+//        Logbook logDay200=new Logbook(12, 2000);
+//
+//
+//        for (day = 1; day <=logDay100.daysInMonth(); day++) 
+//        {
+//            logDay100.putEntry(day, day*100);
+//            logDay200.putEntry(day, day*200);
+//        }
+//
+//
+//        logDay100.plus(logDay200);
+//
+//        System.out.println("Month/Year : "+logDay200.getLogMonth()+"/"+logDay200.getLogYear());
+//        for (day = 1; day <= logDay100.daysInMonth(); day++) 
+//        {
+//            System.out.println(day + " : "+logDay100.getEntry(day));   
+//        }
+              
+              
+//------------------------Own written Leap Year implementation------------------
+
+//        Logbook leap=new Logbook(2, 2000);
+//        Logbook notLeap=new Logbook(2, 1999);
+//
+//        System.out.println(leap.isLeapYear(leap.getLogYear()));
+//        System.out.println(leap.isLeapYear(notLeap.getLogYear()));
+    }
+    
+    
     
 }
--- src/logbook/Logbook.java
+++ src/logbook/Logbook.java
@@ -1,26 +1,46 @@
 package logbook;
 
 import java.io.*;
+import java.time.Month;
 import java.util.*;
-
+import java.time.YearMonth;
 
 public class Logbook {
     private int logMonth,logYear;
     private int[] entry=new int[31];
     private GregorianCalendar logCalendar;
 
+    public Logbook() {
+        logCalendar=new GregorianCalendar();
+        logYear=logCalendar.get(Calendar.YEAR);
+        logMonth=logCalendar.get(Calendar.MONTH)+1;
+    }
+    
+    
     public Logbook(int logMonth, int logYear) {
-        this.logMonth = logMonth;
-        this.logYear = logYear;
+        
+        this.logYear=logYear;
+        this.logMonth=logMonth>=1 && logMonth<=12 ? logMonth : 1;
+        logCalendar=new GregorianCalendar(logYear, logMonth-1, 1);
+    }
+    public void putEntry(int value)
+    {
+        entry[logCalendar.get(Calendar.DAY_OF_MONTH)]=value;
     }
     
     public void putEntry(int day , int value)
     {
-        entry[day]=value;
+        entry[day-1]=value;
     }
+    
     public int getEntry(int day)
     {
-        return entry[day];
+        return entry[day-1];
+    }
+    
+    public int getEntry()
+    {
+        return entry[logCalendar.get(Calendar.DAY_OF_MONTH)];
     }
 
     public int getLogMonth() {
@@ -33,7 +53,9 @@ public class Logbook {
     
     public int daysInMonth()
     {
-        return 0;
+        YearMonth object=YearMonth.of(logYear, logMonth);
+        int daysInMonth=object.lengthOfMonth();
+        return daysInMonth;
     }
     
     
@@ -41,6 +63,40 @@ public class Logbook {
     {
         return logCalendar.isLeapYear(logYear);
     }
-
+    
+    public int dayOfWeek(int day)
+    {
+        logCalendar.set(logYear, logMonth-1, day);
+        return logCalendar.get(Calendar.DAY_OF_WEEK)-1;
+    }
+    
+    
+    public void plus(Logbook obj)
+    {
+        int day;
+        for ( day = 0; day< obj.daysInMonth(); day++) 
+        {
+            this.entry[day]+=obj.entry[day];
+        }
+    }
+    
+//--------If there were no built in method I would implement it as follows------
+    
+    public boolean isLeapYear(int year)
+    {
+        return year%4==0 || year%100==0 || year% 400==0;
+    }
+    
+    
+//--------If there were no built in method I would implement it as follows------
+    
+    
+    public int day(int year, int month , int day)
+    {
+        int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 }; 
+        year -= (month < 3) ? 1 : 0; 
+        return ( year + year/4 - year/100 + year/400 + t[month-1] + day) % 7;
+    }
+    
     
 }

08168c1c71d4637b4ba23ad17b8e4b4a8a2f6ccb
Asim Habib <de.asimhabib@gmail.com>
2020-10-11 12:38:02 +0500

    Completed class definition of Logbook and created COffee class

--- src/logbook/Logbook.java
+++ src/logbook/Logbook.java
@@ -1,9 +1,46 @@
-
 package logbook;
+
+import java.io.*;
+import java.util.*;
 
 
 public class Logbook {
+    private int logMonth,logYear;
+    private int[] entry=new int[31];
+    private GregorianCalendar logCalendar;
 
+    public Logbook(int logMonth, int logYear) {
+        this.logMonth = logMonth;
+        this.logYear = logYear;
+    }
+    
+    public void putEntry(int day , int value)
+    {
+        entry[day]=value;
+    }
+    public int getEntry(int day)
+    {
+        return entry[day];
+    }
+
+    public int getLogMonth() {
+        return logMonth;
+    }
+
+    public int getLogYear() {
+        return logYear;
+    }
+    
+    public int daysInMonth()
+    {
+        return 0;
+    }
+    
+    
+    private boolean leapYear()
+    {
+        return logCalendar.isLeapYear(logYear);
+    }
 
     
 }
